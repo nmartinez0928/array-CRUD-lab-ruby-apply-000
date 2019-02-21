8d6ea8eee9ea0a05ff09b976d0a4c59b503a588b
Natalie Martinez <nmartinez0928@gmail.com>
2019-02-21 03:16:14 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,45 @@
 def create_an_empty_array
+  []
+end
   
 end
 
 def create_an_array
+  var fruits = ["Banana", "Orange", "Apple", "Mango"];
   
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  var fruits = ["banana", "Orange, "Apple", "Mango"];
+  fruits.push("Kiwi");
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  var fruits = ["banana", "Orange, "Apple", "Mango"];
+  fruits.unshift("Kiwi");
 end
 
 def remove_element_from_end_of_array(array)
+  var fruits = ["banana", "Orange, "Apple", "Mango"];
+  fruits.pop("Kiwi");
   
 end
 
 def remove_element_from_start_of_array(array)
+  var fruits = ["banana", "Orange, "Apple", "Mango"];
+  fruits.shift();
   
 end
 
 def retrieve_element_from_index(array, index_number)
+  String[] fruits = {"banana", "Orange", "Apple", "Mango"};
   
 end
 
 def retrieve_first_element_from_array(array)
-  
+  fruits[0] = "banana";
 end
 
 def retrieve_last_element_from_array(array)
-  
+  fruits[0] = "banana" -1;
 end
