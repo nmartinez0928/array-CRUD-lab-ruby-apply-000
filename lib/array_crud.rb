191ad3fa58538d9e0082829609ddbab1bc783760
Natalie Martinez <nmartinez0928@gmail.com>
2019-02-21 02:23:49 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -20,7 +20,8 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  
+  var fruits = ["banana", "Orange, "Apple", "Mango"];
+  fruits.pop("Kiwi");
 end
 
 def remove_element_from_start_of_array(array)
