79b0d0aac069c930c9fe065c9b78051d125afcf7
Natalie Martinez <nmartinez0928@gmail.com>
2019-02-21 02:17:52 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -11,6 +11,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   var fruits = ["banana", "Orange, "Apple", "Mango"];
+  fruits.push
 end
 
 def add_element_to_start_of_array(array, element)
